ddb63cd38906bb80945aa0d84963252d446f1657
Alexis-Yovo <74179398+Alexis-Yovo@users.noreply.github.com>
2025-07-16 09:24:50 +0200

    Create tab.php

---
+++ tab.php
@@ -0,0 +1,35 @@
+<?php
+// Tableaux associatifs représentant les personnes
+$personne1 = [
+    "nom" => "Ganesb",
+    "age" => 52,
+    "sexe" => "Homme"
+];
+
+$personne2 = [
+    "nom" => "Sarah",
+    "age" => 28,
+    "sexe" => "Femme"
+];
+
+$personne3 = [
+    "nom" => "Gildas",
+    "age" => 38,
+    "sexe" => "Homme"
+];
+
+// Tableau contenant toutes les personnes
+$toutesLesPersonnes = [$personne1, $personne2, $personne3];
+
+// Fonction pour afficher une personne
+function afficherPersonne($personne) {
+    echo "Nom : " . $personne["nom"] . "<br>";
+    echo "Âge : " . $personne["age"] . "<br>";
+    echo "Sexe : " . $personne["sexe"] . "<br><hr>";
+}
+
+// Appel de la fonction pour afficher toutes les personnes
+foreach ($toutesLesPersonnes as $personne) {
+    afficherPersonne($personne);
+}
+?>
